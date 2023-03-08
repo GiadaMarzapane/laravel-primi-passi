064c9bb77a5724c35a59c3b8971cbe8c355153b9
GiadaMarzapane <giada.ortesta@gmail.com>
2023-03-08 18:19:01 +0100

    creata seconda pagina (/scarpe) e suo href

--- resources/views/shoes.blade.php
+++ resources/views/shoes.blade.php
@@ -4,11 +4,10 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=Laravel Primi Passi, initial-scale=1.0">
-
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 
-    <title>Laravel Primi Passi</title>
+    <title>Scarpe</title>
 </head>
 
 <body>
@@ -20,7 +19,9 @@
             <nav>
                 <ul>
                     <li class="list-inline-item me-3">
-                        <a class="text-decoration-none text-dark text-capitalize" href="scarpe">scarpe</a>
+                        <a class="text-decoration-none text-dark text-capitalize" href="">
+                            <strong>scarpe</strong>
+                        </a>
                     </li>
                     <li class="list-inline-item me-3">
                         <a class="text-decoration-none text-dark text-capitalize" href="">nuovi arrivi</a>
@@ -44,6 +45,33 @@
             </nav>
         </div>
     </header>
+    <main class="mt-4">
+        <div class="d-flex justify-content-around align-items-center">
+            <div>
+                <p>
+                    <strong class="text-uppercase">Iscriviti alla newsletter</strong> - Subito te un <strong class="text-uppercase">-10%</strong>
+                </p>
+            </div>
+            <div>
+                <p>
+                    <strong class="text-uppercase">-15% su tutto</strong> - Codice: <strong class="text-uppercase">hello15</strong>
+                </p>
+            </div>
+            <div>
+                <p>
+                    <strong class="text-uppercase">spedizione gratuita</strong> - per ordini superiori a <strong class="text-uppercase">€70</strong>
+                </p>
+            </div>
+        </div>
+        <section>
+            <div class="text-center mt-5">
+                <p>
+                    <strong>825</strong> prodotti trovati
+                </p>
+                <p>&#128151; Metti mi piace ai tuoi prodotti preferiti</p>
+            </div>
+        </section>
+    </main>
 </body>
 
 </html>
